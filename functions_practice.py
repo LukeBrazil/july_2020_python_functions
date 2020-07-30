0ea4277841baa8dea776935cffb21542e38e9aa2
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-30 13:34:25 -0500

    Python Functions Practice

---
+++ functions_practice.py
@@ -0,0 +1,17 @@
+def add(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def convertCase(argument):
+    return argument.upper()
+
+result = add(2, 2)
+difference = minus(4, 1)
+
+# print(result)
+# print(difference)
+
+title = convertCase("The great gatsby")
+print(title)
